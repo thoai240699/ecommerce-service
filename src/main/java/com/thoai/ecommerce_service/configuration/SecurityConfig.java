aa6f4c12e0875b02356faf1c2fe4858804853c46
thoai240699 <thoai240699@gmail.com>
2025-06-01 13:40:53 +0700

    fix issue PUBLIC_ENDPONTS

--- src/main/java/com/thoai/ecommerce_service/configuration/SecurityConfig.java
+++ src/main/java/com/thoai/ecommerce_service/configuration/SecurityConfig.java
@@ -18,7 +18,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
-    private final String[] PUBLIC_ENDPOINTS = {
+    private static final String[] PUBLIC_ENDPOINTS = {
         "/users", "/auth/token", "/auth/introspect", "/auth/logout",
     };
 
